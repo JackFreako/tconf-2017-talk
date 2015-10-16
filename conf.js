ce94bd74eb3126a0847c4c16e69c8397cd35ea3a
Jags <jags14385@users.noreply.github.com>
2015-10-16 20:42:08 +1100

    Added .travis.yml

--- conf.js
+++ conf.js
@@ -3,10 +3,11 @@ exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: [
 	'*-spec.js'],
-	
+
 	capabilities:{
 		//browserName: 'phantomjs', 'phantomjs.binary.path': require('phantomjs').path,
 		browserName: 'chrome',
 		shardTestFiles: true,
-		maxInstances: 2}
-	};
+		maxInstances: 2
+	}
+};
