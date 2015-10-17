f45e642ac2c6a5c3742dabdba99b26ece5d248c0
Jags <jags14385@users.noreply.github.com>
2015-10-17 17:00:22 +1100

    Moved the spec files to e2e folder and enabled direct connect .

--- conf.js
+++ conf.js
@@ -1,13 +1,13 @@
 exports.config = {
 	framework: 'jasmine2',
 	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: [
-	'*-spec.js'],
+	specs: ['e2e/*-spec.js'],
 
 	capabilities:{
 		//browserName: 'phantomjs', 'phantomjs.binary.path': require('phantomjs').path,
 		browserName: 'chrome',
 		shardTestFiles: true,
 		maxInstances: 2
-	}
+	},
+	directConnect: true
 };
